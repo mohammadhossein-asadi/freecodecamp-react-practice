6b53c6359d1487192824b2f3f3acbe3b21159a5a
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-04 21:14:20 +0430

    commit

---
+++ src/components/ClosingTag.js
@@ -0,0 +1,12 @@
+const ClosingTag = () => {
+  const JSX = (
+    <div>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <hr />
+    </div>
+  );
+  return <div>{JSX}</div>;
+};
+
+export default ClosingTag;
