7f5a798a4366a4421e38a62e6dc792ac6ec3d3a9
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-04 21:11:33 +0430

    commit

---
+++ src/components/HTmlClassInJSX.js
@@ -0,0 +1,10 @@
+const HTmlClassInJSX = () => {
+  const JSX = (
+    <div className="myDiv">
+      <h1>Add a class to this div</h1>
+    </div>
+  );
+  return <div>{JSX}</div>;
+};
+
+export default HTmlClassInJSX;
