eeb393c7e1b9b21093d1cfea6e8e367c2106e538
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-04 20:57:03 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import SimpleJsxElement from "./components/SimpleJsxElement";
 import ComplexJSXElement from "./components/ComplexJSXElement";
+import RenderHtmlElement from "./components/RenderHtmlElement";
 
 function App() {
   return (
     <>
       <SimpleJsxElement />
       <ComplexJSXElement />
+      <RenderHtmlElement />
     </>
   );
 }
