4dd34524d5a771a9020f63d9ffa574d8670bc9da
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-04 20:40:36 +0430

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleJsxElement from "./components/SimpleJsxElement";
+
+function App() {
+  return (
+    <>
+      <SimpleJsxElement />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SimpleJsxElement.js
@@ -0,0 +1,6 @@
+const SimpleJsxElement = () => {
+  const JSX = <h1>Hello JSX</h1>;
+  return <div>{JSX}</div>;
+};
+
+export default SimpleJsxElement;
